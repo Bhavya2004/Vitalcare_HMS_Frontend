40546a0f40bf633389f3017856c25367bc47c701
Bhavya Barai <bhavya.barai@simformsolutions.com>
2025-05-02 11:57:43 +0530

    fix: created seperate files for interfaces and improved  naming convention

--- src/app/core/services/appointment.service.ts
+++ src/app/core/services/appointment.service.ts
@@ -2,51 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { ApiResponse, Appointment, CreateAppointmentData, Doctor } from '../../shared/models/appointment.model';
 
-export interface Doctor {
-  id: string;
-  name: string;
-  specialization: string;
-  img?: string;
-}
-
-export interface Patient {
-  first_name: string;
-  last_name: string;
-  gender: string;
-  phone: string;
-  address: string;
-  date_of_birth: string;
-}
-
-export interface Appointment {
-  id: number;
-  patient_id: string;
-  doctor_id: string;
-  appointment_date: string;
-  time: string;
-  status: 'PENDING' | 'SCHEDULED' | 'CANCELLED' | 'COMPLETED';
-  type: string;
-  note?: string;
-  reason?: string;
-  doctor: Doctor;
-  created_at: string;
-  patient: Patient;
-}
-
-export interface CreateAppointmentData {
-  doctor_id: string;
-  appointment_date: string;
-  time: string;
-  type: string;
-  note?: string;
-}
-
-export interface ApiResponse<T> {
-  success: boolean;
-  message?: string;
-  data: T;
-}
 
 @Injectable({
   providedIn: 'root',
--- src/app/features/patient/appointments/appointment-detail/appointment-detail.component.ts
+++ src/app/features/patient/appointments/appointment-detail/appointment-detail.component.ts
@@ -17,9 +17,9 @@ export class AppointmentDetailComponent {
     const today = new Date();
     const birthDate = new Date(dateOfBirth);
     let age = today.getFullYear() - birthDate.getFullYear();
-    const m = today.getMonth() - birthDate.getMonth();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
     
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
       age--;
     }
     
